c8b184761322ca0ef7f2d69e32173750cb3e5782
Kevin Miller <kmillergit@outlook.com>
2020-08-22 18:20:14 -0700

    file read json from api works

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
 
 const app = express();
 const PORT = 3000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", function(req, res) {
+
+app.get("/notes", function(request, response) {
+    response.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", "utf8", function(error, notesObj) {
+        if (error) {
+            throw new Error ("File read error");
+        }
+        notesObj = JSON.parse(notesObj);
+        console.log(notesObj);
+        // notesObj = JSON.stringify(notesObj)
+        res.json(notesObj)
+    })
+})
+
+// this must be last, as the default action
+app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
